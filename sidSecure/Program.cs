9828382b10f02c2ef14fb18207a9c6fa209fdaa8
goldblackk <45857590+goldblackk@users.noreply.github.com>
2020-05-03 12:25:50 +0200

    Add files via upload

---
+++ sidSecure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using System.DirectoryServices;
+using System.Net;
+
+namespace sidSecure
+{
+    class Program
+    {
+        public static SecurityIdentifier GetSID()
+        {
+            return new SecurityIdentifier((byte[])new DirectoryEntry(string.Format("WinNT://{0},Computer", Environment.MachineName)).Children.Cast<DirectoryEntry>().First().InvokeGet("objectSID"), 0).AccountDomainSid;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "sidSecure - SID Secure System by goldblack";
+            string SID = GetSID().ToString();
+            string list = new WebClient()
+            {
+                Proxy = ((IWebProxy)null)
+            }.DownloadString("https://pastebin.com/raw/YK5XWGyv");
+            if (list.Contains(SID))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Successfully authenticated. SID: (" + SID + ")");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Authentication failure. SID: (" + SID + ")");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
