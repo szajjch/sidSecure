c8edac7c729dcf8b74f7524a8611b5d38c2695e9
goldblackk <45857590+goldblackk@users.noreply.github.com>
2020-05-06 11:55:28 +0200

    Add files via upload

--- sidSecure/Program.cs
+++ sidSecure/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security.Principal;
 using System.DirectoryServices;
-using System.Net;
+using MySql.Data.MySqlClient;
 
 namespace sidSecure
 {
@@ -19,21 +17,112 @@ namespace sidSecure
         {
             Console.Title = "sidSecure - SID Secure System by goldblack";
             string SID = GetSID().ToString();
-            string list = new WebClient()
+            new MySQLConnect(SID);
+        }
+    }
+
+    class MySQLConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public MySQLConnect(string SID)
+        {
+            Start(SID);
+        }
+
+        private void Start(string SID)
+        {
+            server = "localhost"; // CHANGE ME
+            database = "sid";
+            uid = "root"; // CHANGE ME
+            password = ""; // CHANGEME
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            Select(SID);
+        }
+        private bool OpenConnection()
+        {
+            try
             {
-                Proxy = ((IWebProxy)null)
-            }.DownloadString("https://pastebin.com/raw/YK5XWGyv");
-            if (list.Contains(SID))
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Successfully authenticated. SID: (" + SID + ")");
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: Cannot connect to server.");
+                        break;
+
+                    case 1045:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: Unknown error.");
+                        break;
+                }
+                return false;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public List<string>[] Select(string SID)
+        {
+            string query = "SELECT * FROM sidsecure";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["name"] + "");
+                    list[1].Add(dataReader["sid"] + "");
+                }
+
+                if (list[1].Contains(SID))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Successfully authenticated. SID: (" + SID + ")");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Authentication failure. SID: (" + SID + ")");
+                }
+                Console.ReadKey(true);
+                dataReader.Close();
+                this.CloseConnection();
+                return list;
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Authentication failure. SID: (" + SID + ")");
+                return list;
             }
-            Console.ReadKey(true);
         }
     }
 }
